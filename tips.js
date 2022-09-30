2d3ef37cd9183916696029706c3685e3de907f19
Leslit Molina <113367291+lmolina613@users.noreply.github.com>
2022-09-30 14:35:16 -0400

    complete exercise

---
+++ tips.js
@@ -0,0 +1,13 @@
+// sample inputs
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.00 ;
+
+// calculations
+var totalDue = foodCost + tax + tip;
+// output
+console.log('the total is ' + totalDue  )
+console.log(
+    "Food cost is " + foodCost + " and tax is " + tax);
+console.log(foodCost + " $$$")
+console.log(tip.toFixed(2))
